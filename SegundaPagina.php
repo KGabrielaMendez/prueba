9b174cc73d1d7b89a7923b60414fe0722fe797ce
KGabrielaMendez <kgmendezg@utn.edu.ec>
2018-06-23 22:24:48 -0500

    agregue otra pagina

---
+++ SegundaPagina.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Esto que es</title>
+    </head>
+    <body>
+es una prueba :D
+    </body>
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
